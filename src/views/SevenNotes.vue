bfd48f342af1fce7e15aafb6801fddba961a08ca
Jale Ismayilova <zhala-ismayilova@mail.ru>
2025-01-26 17:34:25 +0400

    mobile fixing background

--- src/views/SevenNotes.vue
+++ src/views/SevenNotes.vue
@@ -566,8 +566,8 @@ li {
     position: fixed;
     top: 0;
     left: 0;
-    width: 100vw;
-    height: 100vh;
+    /*width: 100vw;*/
+    /*height: 100vh;*/
     background: url('../assets/roses-bg.jpg') no-repeat center center;
     /*background-size: cover;*/
     transform: rotate(90deg);
